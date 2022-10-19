4b48c9bbe8a4b0eaa2214ecef5e234c54b1dcdd3
Adityapewe <adityapria8@gmail.com>
2022-10-19 16:01:51 +0700

    Controller Introduction

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    private $name = "Aditya Pria Wiguna";
+
+    public function index(){
+        return $this->name;
+    }
+
+    public function show($param){
+        $this->name = $param;
+        return $this->name;
+    }
+
+
+}
