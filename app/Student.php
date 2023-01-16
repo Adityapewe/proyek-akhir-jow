440521dba6e78b38c2f49732c5f87556934399f1
Adityapewe <adityapriaw8@gmail.com>
2023-01-16 16:25:32 +0700

    crud sesi13

--- app/Student.php
+++ app/Student.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $fillable = [
+        'code', 'name', 'gender', 'birth_place', 'birth_date',
+    ];
 }
--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -2,6 +2,16 @@
 
 <?php $no=1 ?>
 @section('first.content')
+    <h3>Data Mahasisawa</h3>    
+        <a href="/student/create" class="btn btn-success">Tambah Data</a>
+        <div class="col-sm-12">
+        
+        @if(session()->get('success'))
+        <div class="alert alert-success">
+            {{ session()->get('success')}}
+        </div>
+        @endif
+        </div>
 
     <table class="table table-striped">
         <thead>
@@ -12,6 +22,7 @@
                 <th> Jenis Kelamin </th>
                 <th> Tempat Lahir </th>
                 <th> Tanggal Lahir </th>
+                <th colspan=2> </th>
             </tr>
         </thead>
         <tbody>
@@ -23,6 +34,16 @@
                     <td> {{$student->gender == "P" ? "Pria" : "Wanita"}} </td>
                     <td> {{$student->birth_date}} </td>
                     <td> {{$student->birth_place}} </td>
+                    <td>
+                        <a href="/student/edit/{{$student->id}}" method="post" class="btn btn-primary">Edit</a>
+                    </td>
+                    <td>
+                        <form action="/student/{{$student->id}}/delete" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger" type="submit">Delete</button>
+                        </form>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
